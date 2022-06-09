1049b42981601b3ee8a947b18ae74788e499da1f
yas-chal <yasserchalal@gmail.com>
2022-06-09 12:12:21 +0100

    first commit

---
+++ src/components/profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Adress = () => {
+    return (
+        <div>
+            <span>Adress : Bejaia ,06000 ,Algerie</span>
+        </div>
+    );
+};
+
+export default Adress;
---
+++ src/components/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src="/photoprofile.jpeg" className='img' />
+        </div>
+    );
+};
+
+export default ProfilePhoto;
